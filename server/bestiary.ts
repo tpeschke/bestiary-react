9bcbd08db6122959c7a2a13c7addf0bdb8b6fb3b
Trent <mr.peschke@gmail.com>
2025-03-19 19:47:59 -0600

    updating the authentication

--- server/bestiary.ts
+++ server/bestiary.ts
@@ -2,16 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import massive from 'massive'
-import session from 'express-session'
-import passport from 'passport'
-import Auth0Strategy from 'passport-auth0'
 import path from 'path'
 
-import { Profile, Response } from './interfaces'
+import { Response } from './interfaces'
 
 import { sendErrorForwardNoFile } from './helpers/sendingFunctions'
 
-import { server, databaseCredentials, secret, domain, clientID, clientSecret, callbackURL, fakeAuth } from './server-config'
+import { server, databaseCredentials } from './server-config'
+
+import authRoutes from './routes/authentication'
 
 const sendErrorForward = sendErrorForwardNoFile('main server')
 
@@ -19,53 +18,16 @@ const app = express()
 app.use(bodyParser.json({ limit: '10mb' }))
 app.use(cors())
 
-app.use(express.static(__dirname + `/../dist/bestiary`));
+// app.use(express.static(__dirname + `/../dist/bestiary`));
 
 // ================================== \\
 
-app.use(session({
-    secret,
-    resave: false,
-    saveUninitialized: true
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new Auth0Strategy({
-    domain,
-    clientID,
-    clientSecret,
-    callbackURL,
-    scope: 'openid profile'
-}, async (accessToken: string, refreshToken: string, extraParams: Object, profile: Profile, finishingCallback: Function) => {
-    const { displayName, user_id: userID } = profile;
-    const db = app.get('db');
-    const [ user ] = await db.find.user(userID)
-    if (!user) {
-        await db.create.user(displayName, userID)
-    }
-    return finishingCallback(null, user.id)
-}))
-
-app.use(fakeAuth)
-
-app.get('/auth', passport.authenticate('auth0'));
-app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: `/`
-}));
-
-passport.serializeUser((id : number, done : Function) => {
-    done(null, id)
-})
-passport.deserializeUser(async (id : number, done : Function) => {
-    const [ user ] = await app.get('db').user.findSession(id)
-    return done(null, user);
-})
+app.use('/', authRoutes)
 
 // ================================== \\
 
 app.get('/*', (_, response : Response) => {
-    response.sendFile(path.join(__dirname + '/../dist/bestiary/index.html'))
+    // response.sendFile(path.join(__dirname + '/../dist/bestiary/index.html'))
 })
 
 // ================================== \\
---
+++ server/routes/authentication.ts
@@ -0,0 +1,51 @@
+import express from 'express'
+import session from 'express-session'
+import passport from 'passport'
+import Auth0Strategy from 'passport-auth0'
+
+import { secret, domain, clientID, clientSecret, callbackURL, fakeAuth } from '../server-config'
+
+import { Profile } from '../interfaces'
+
+const authRoutes = express.Router()
+
+authRoutes.use(session({
+    secret,
+    resave: false,
+    saveUninitialized: true
+}))
+authRoutes.use(passport.initialize());
+authRoutes.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain,
+    clientID,
+    clientSecret,
+    callbackURL,
+    scope: 'openid profile'
+}, async (accessToken: string, refreshToken: string, extraParams: Object, profile: Profile, finishingCallback: Function) => {
+    const { displayName, user_id: userID } = profile;
+    const db = authRoutes.get('db');
+    const [ user ] = await db.find.user(userID)
+    if (!user) {
+        await db.create.user(displayName, userID)
+    }
+    return finishingCallback(null, user.id)
+}))
+
+authRoutes.use(fakeAuth)
+
+authRoutes.get('/auth', passport.authenticate('auth0'));
+authRoutes.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: `/`
+}));
+
+passport.serializeUser((id : number, done : Function) => {
+    done(null, id)
+})
+passport.deserializeUser(async (id : number, done : Function) => {
+    const [ user ] = await authRoutes.get('db').user.findSession(id)
+    return done(null, user);
+})
+
+export default authRoutes
