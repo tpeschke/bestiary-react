2a065f2bb03d45c9f04069b983fc70ec83a3ea1c
Trent <mr.peschke@gmail.com>
2025-04-13 17:48:40 -0600

    expanding unit tests

--- src/tests/src/components/beast/models/PlayerBeastClass.test.tsx
+++ src/tests/src/components/beast/models/PlayerBeastClass.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
-import PlayerBeastClass from '../../../../../components/beast/models/PlayerBeastClass';
-import { PlayerBeastInfo } from '../../../../../components/beast/interfaces/viewInterfaces';
 import { getRandomNumber, getRandomString } from '../../../../utilities/randomGenerator';
+import { PlayerBeastInfo } from '../../../../../pages/beast/interfaces/viewInterfaces';
+import PlayerBeastClass from '../../../../../pages/beast/models/PlayerBeastClass';
 
 describe('Blank PlayerBeastClass', () => {
     const emptyTestClass: PlayerBeastInfo = {
@@ -9,7 +9,8 @@ describe('Blank PlayerBeastClass', () => {
         name: '',
         notes: {
             notes: ''
-        }
+        },
+        artistInfo: {}
     }
     let testClass = new PlayerBeastClass(emptyTestClass)
 
@@ -26,21 +27,45 @@ describe('Blank PlayerBeastClass', () => {
         expect(testClass.notes.notes).toBe('')
     })
 
+    test('Returns artist info', () => {
+        expect(testClass.artistInfo.id).toBe(undefined)
+        expect(testClass.artistInfo.artistid).toBe(undefined)
+        expect(testClass.artistInfo.artist).toBe(undefined)
+        expect(testClass.artistInfo.tooltip).toBe(undefined)
+        expect(testClass.artistInfo.link).toBe(undefined)
+        expect(testClass.artistInfo.roleid).toBe(undefined)
+    })
+
     test('Returns all info', () => {
         const returnedInfo = testClass.info
 
         expect(returnedInfo.id).toBe(0)
         expect(returnedInfo.name).toBe('')
+
         expect(returnedInfo.notes.id).toBe(undefined)
         expect(returnedInfo.notes.notes).toBe('')
+
+        expect(testClass.artistInfo.id).toBe(undefined)
+        expect(testClass.artistInfo.artistid).toBe(undefined)
+        expect(testClass.artistInfo.tooltip).toBe(undefined)
+        expect(testClass.artistInfo.link).toBe(undefined)
+        expect(testClass.artistInfo.roleid).toBe(undefined)
     })
 })
 
 describe('PlayerBeastClass', () => {
     const expectedId = getRandomNumber()
     const expectedName = getRandomString()
+
     const expectedNotesId = getRandomNumber()
     const expectedNotes = getRandomString()
+
+    const expectedArtistDBId = getRandomNumber()
+    const expectedArtistId = getRandomNumber()
+    const expectedArtist = getRandomString()
+    const expectedTooltip = getRandomString()
+    const expectedLink = getRandomString()
+    const expectedRoleId = getRandomString()
 
     const emptyTestClass: PlayerBeastInfo = {
         id: expectedId,
@@ -48,12 +73,20 @@ describe('PlayerBeastClass', () => {
         notes: {
             id: expectedNotesId,
             notes: expectedNotes
+        },
+        artistInfo: {
+            id: expectedArtistDBId,
+            artistid: expectedArtistId,
+            artist: expectedArtist,
+            tooltip: expectedTooltip,
+            link: expectedLink,
+            roleid: expectedRoleId
         }
     }
     let testClass = new PlayerBeastClass(emptyTestClass)
 
     beforeAll(() => {
-        jest.spyOn(PlayerBeastClass.prototype as any, 'saveNotes').mockImplementation(() => {});
+        jest.spyOn(PlayerBeastClass.prototype as any, 'saveNotes').mockImplementation(() => { });
     });
 
     test('Gets id', () => {
@@ -62,6 +95,15 @@ describe('PlayerBeastClass', () => {
 
     test('Gets name', () => {
         expect(testClass.name).toBe(expectedName)
+    })
+
+    test('Returns artist info', () => {
+        expect(testClass.artistInfo.id).toBe(expectedArtistDBId)
+        expect(testClass.artistInfo.artistid).toBe(expectedArtistId)
+        expect(testClass.artistInfo.artist).toBe(expectedArtist)
+        expect(testClass.artistInfo.tooltip).toBe(expectedTooltip)
+        expect(testClass.artistInfo.link).toBe(expectedLink)
+        expect(testClass.artistInfo.roleid).toBe(expectedRoleId)
     })
 
     test('Gets notes', () => {
@@ -74,15 +116,23 @@ describe('PlayerBeastClass', () => {
 
         expect(returnedInfo.id).toBe(expectedId)
         expect(returnedInfo.name).toBe(expectedName)
+
         expect(returnedInfo.notes.id).toBe(expectedNotesId)
         expect(returnedInfo.notes.notes).toBe(expectedNotes)
+
+        expect(testClass.artistInfo.id).toBe(expectedArtistDBId)
+        expect(testClass.artistInfo.artistid).toBe(expectedArtistId)
+        expect(testClass.artistInfo.artist).toBe(expectedArtist)
+        expect(testClass.artistInfo.tooltip).toBe(expectedTooltip)
+        expect(testClass.artistInfo.link).toBe(expectedLink)
+        expect(testClass.artistInfo.roleid).toBe(expectedRoleId)
     })
 
     test('Set Notes', () => {
         const newExpectedNotes = getRandomString()
 
         testClass.setNotes(newExpectedNotes)
-        
+
         expect(testClass.notes.id).toBe(expectedNotesId)
         expect(testClass.notes.notes).toBe(newExpectedNotes)
     })
