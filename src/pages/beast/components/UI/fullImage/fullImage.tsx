fe8f8c9ddd49478784a98cc000c0cf0a54f6f3af
Trent <mr.peschke@gmail.com>
2025-07-01 06:44:00 -0600

    adding a 404 image

--- src/pages/beast/components/UI/fullImage/fullImage.tsx
+++ src/pages/beast/components/UI/fullImage/fullImage.tsx
@@ -1,4 +1,5 @@
 import './fullImage.css'
+import ImageNotFound from '../../../../../assets/images/404.png'
 
 import { ArtistInfo } from '../../../interfaces/infoInterfaces/ImageInfoInterfaces'
 
@@ -15,9 +16,14 @@ export default function FullImage({ imageParam, altText, artistInfo }: Props) {
     const tooltip = artistInfo?.tooltip
     const artist = artistInfo?.artist
 
+    function handleImageError({currentTarget}: any) {
+        currentTarget.onerror = null
+        currentTarget.src = ImageNotFound
+    }
+
     return (
         <>
-            <img src={imageBase + imageParam} alt={altText}></img>
+            <img src={imageBase + imageParam} alt={altText} onError={handleImageError}></img>
             <div className='artist-frame'>
                 {link ?
                     <a target="_blank" href={link} data-tooltip-id="my-tooltip" data-tooltip-content={tooltip}>{artist}</a>
--- src/pages/catalog/components/tile/Tile.tsx
+++ src/pages/catalog/components/tile/Tile.tsx
@@ -1,4 +1,5 @@
 import './Tile.css'
+import ImageNotFound from '../../../../assets/images/404.png'
 
 import { Link } from "react-router-dom";
 
@@ -13,12 +14,17 @@ interface Props {
 export default function Tile({ tile }: Props) {
     const { id, thumbnail, name, canplayerview, patreon, notupdating } = tile
 
+    function handleImageError({currentTarget}: any) {
+        currentTarget.onerror = null
+        currentTarget.src = ImageNotFound
+    }
+
     const tooltip = notupdating ? "This entry isn't being updated currently. If you need it, let Peschke know and he'll update it for you. \nYou can still view it." : null
     return (
         <Link to={`/beast/${id}`}>
             <div className={notupdating ? 'tile not-updating' : 'tile'} data-tooltip-id="my-tooltip" data-tooltip-content={tooltip}>
                 <div className='image-frame'>
-                    <img src={imageBase + id} style={{ 'objectPosition': thumbnail ?? 'top' }}></img>
+                    <img src={imageBase + id} style={{ 'objectPosition': thumbnail ?? 'top' }} onError={handleImageError}></img>
                 </div>
                 <span className='name-frame'>
                     <TileIcon canplayerview={canplayerview} patreon={patreon} />
