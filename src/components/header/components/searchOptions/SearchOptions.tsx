4e285cf8597aa9af3939f2b4e4ed7fcc71375187
Trent <mr.peschke@gmail.com>
2025-07-06 13:55:58 -0600

    filling in search values from query

--- src/components/header/components/searchOptions/SearchOptions.tsx
+++ src/components/header/components/searchOptions/SearchOptions.tsx
@@ -1,11 +1,12 @@
 import './SearchOptions.css'
 
-import { createSearchParams, useNavigate } from 'react-router-dom'
+import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom'
 
 import Icon from '../../../icon/Icon'
 
 export default function SearchOptions() {
     const navigate = useNavigate()
+    const [searchParams] = useSearchParams();
 
     function captureQuery(value: string) {
         if (value) {
@@ -15,13 +16,19 @@ export default function SearchOptions() {
             })
         }
     }
+
+    const nameParam: string = getStringValueFromParam(searchParams.get('name'))
     
     return (
         <div className='search-options-shell'>
             <span>
                 <Icon iconName='magnifying-glass' margin='right' color='black' />
-                <input onBlur={e => captureQuery(e.target.value)} placeholder='Search by Name' />
+                <input onBlur={e => captureQuery(e.target.value)} placeholder='Search by Name' value={nameParam} />
             </span>
         </div>
     )
+}
+
+function getStringValueFromParam(paramValue: string | null): string {
+    return paramValue ? paramValue : ''
 }
