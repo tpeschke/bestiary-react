809de2ddff8b484030eb9ae733a386f5167c0273
Trent <mr.peschke@gmail.com>
2025-07-20 09:43:42 -0600

    uncommented some stuff idk

--- server/controllers/gameMaster/utilities/updateUtilities/updateBeast.ts
+++ server/controllers/gameMaster/utilities/updateUtilities/updateBeast.ts
@@ -1,56 +1,66 @@
-// interface BeastRequest extends Request {
-//     body: Beast
-// }
 
-// export async function updateBeast(request: BeastRequest, response: Response) {
-//     const databaseConnection = getDatabaseConnection(request)
-//     const { body, user } = request
+import { Beast } from "../../../../../common/interfaces/beast/beast";
+import { Request, Response } from "../../../../interfaces/apiInterfaces";
+import getDatabaseConnection from "../../../../utilities/databaseConnection";
+import { checkForContentTypeBeforeSending } from "../../../../utilities/sendingFunctions";
 
-//     const { id, patreon, generalInfo, imageInfo, linkedInfo, roleInfo, combatInfo, skillInfo, socialInfo, lootInfo, castingInfo } = body
-//     const { name, plural, intro, habitat, appearance, senses, diet, meta, size, rarity, scenarios, folklores, tables, encounters } = generalInfo
-//     const { thumbnail, imagesource, artistInfo } = imageInfo
-//     const { variants, locations, types, climates } = linkedInfo
-//     const { rolenameorder, defaultrole, roles } = roleInfo
-//     const { sp_atk, sp_def, tactics, combatpoints, combatrole, combatsecondary, vitalityInfo, movements, attacks, defenses } = combatInfo
-//     const { fatigue, notrauma, knockback, singledievitality, noknockback, rollundertrauma, isincorporeal, weaponbreakagevitality, vitality, locationalVitalities } = vitalityInfo
-//     const { panic, stress, skillrole, skillsecondary, skillpoints, atk_skill, def_skill, skills, challenges, obstacles } = skillInfo
-//     const { traitlimit, relationshiplimit, flawlimit, passionlimit, socialrole, socialsecondary, socialpoints, descriptionshare, convictionshare, relationshipshare, atk_conf, def_conf, archetypeInfo, conflicts } = socialInfo
-//     const { hasarchetypes, hasmonsterarchetypes } = archetypeInfo
-//     const { lootnotes, lairLoot, carriedLoot, specificLoots, pleroma } = lootInfo
-//     const { casting, spells } = castingInfo
+interface BeastRequest extends Request {
+    body: Beast
+}
 
-//     const userid = isOwner(user.id) ? null : user.id
+export async function updateBeast(request: BeastRequest, response: Response) {
+    const databaseConnection = getDatabaseConnection(request)
+    const { body, user } = request
+    
+    const { id: beastID } = body 
 
-//     const effectiveTraitLimit = traitlimit > 0 ? traitlimit : null
-//     const effectiveRelationshipLimit = relationshiplimit > 0 ? relationshiplimit : null
-//     const effectiveFlawLimit = flawlimit > 0 ? flawlimit : null
-//     const effectivePassionLimit = passionlimit > 0 ? passionlimit : null
+    //     const { id, patreon, generalInfo, imageInfo, linkedInfo, roleInfo, combatInfo, skillInfo, socialInfo, lootInfo, castingInfo } = body
+    //     const { name, plural, intro, habitat, appearance, senses, diet, meta, size, rarity, scenarios, folklores, tables, encounters } = generalInfo
+    //     const { thumbnail, imagesource, artistInfo } = imageInfo
+    //     const { variants, locations, types, climates } = linkedInfo
+    //     const { rolenameorder, defaultrole, roles } = roleInfo
+    //     const { sp_atk, sp_def, tactics, combatpoints, combatrole, combatsecondary, vitalityInfo, movements, attacks, defenses } = combatInfo
+    //     const { fatigue, notrauma, knockback, singledievitality, noknockback, rollundertrauma, isincorporeal, weaponbreakagevitality, vitality, locationalVitalities } = vitalityInfo
+    //     const { panic, stress, skillrole, skillsecondary, skillpoints, atk_skill, def_skill, skills, challenges, obstacles } = skillInfo
+    //     const { traitlimit, relationshiplimit, flawlimit, passionlimit, socialrole, socialsecondary, socialpoints, descriptionshare, convictionshare, relationshipshare, atk_conf, def_conf, archetypeInfo, conflicts } = socialInfo
+    //     const { hasarchetypes, hasmonsterarchetypes } = archetypeInfo
+    //     const { lootnotes, lairLoot, carriedLoot, specificLoots, pleroma } = lootInfo
+    //     const { casting, spells } = castingInfo
 
-//     let beastId = id ?? null
-//     if (beastId) {
-//         await databaseConnection.beast.update(beastId, name, intro, habitat, appearance, senses, diet, meta, sp_atk, sp_def, tactics, size, patreon,
-//             vitality, panic, stress, lootnotes, effectiveTraitLimit, effectiveRelationshipLimit, effectiveFlawLimit,
-//             effectivePassionLimit, plural, thumbnail, rarity, combatrole, combatpoints, socialrole, socialpoints, combatsecondary, skillrole, skillpoints, fatigue,
-//             defaultrole, socialsecondary, notrauma, knockback, singledievitality, noknockback, rolenameorder, descriptionshare, convictionshare, relationshipshare, rollundertrauma,
-//             imagesource, isincorporeal, weaponbreakagevitality, hasarchetypes, hasmonsterarchetypes, skillsecondary, atk_skill, def_skill, atk_conf, def_conf)
-//             .catch((error: Error) => sendErrorForward('update main', error, response))
-//     } else {
-//         beastId = await databaseConnection.beast.add(userid, name, intro, habitat, appearance, senses, diet, meta, sp_atk, sp_def, tactics, size, patreon,
-//             vitality, panic, stress, createHash(), lootnotes, effectiveTraitLimit, effectiveRelationshipLimit, effectiveFlawLimit,
-//             effectivePassionLimit, plural, thumbnail, rarity, combatrole, combatpoints, socialrole, socialpoints, combatsecondary, skillrole, skillpoints, fatigue,
-//             defaultrole, socialsecondary, notrauma, knockback, singledievitality, noknockback, rolenameorder, descriptionshare, convictionshare, relationshipshare, rollundertrauma,
-//             imagesource, isincorporeal, weaponbreakagevitality, hasarchetypes, hasmonsterarchetypes, skillsecondary, atk_skill, def_skill, atk_conf, def_conf)
-//             .catch((error: Error) => sendErrorForward('add main', error, response))[0].id
-//     }
+    //     const userid = isOwner(user.id) ? null : user.id
 
-//     const updateParameters: upsertParameters = {
-//         roles, types, climates, attacks, defenses, conflicts, skills, movements, variants, specificLoots, pleroma, locationalVitalities, locations, artistInfo, scenarios, folklores,
-//         casting, deletedSpells, spells, obstacles, challenges, tables, encounters, lairLoot, carriedLoot
-//     }
+    //     const effectiveTraitLimit = traitlimit > 0 ? traitlimit : null
+    //     const effectiveRelationshipLimit = relationshiplimit > 0 ? relationshiplimit : null
+    //     const effectiveFlawLimit = flawlimit > 0 ? flawlimit : null
+    //     const effectivePassionLimit = passionlimit > 0 ? passionlimit : null
 
-//     if (beastId) {
-//         await upsertBeast(databaseConnection, beastId, response, updateParameters)
-//     }
+    //     let beastId = id ?? null
+    //     if (beastId) {
+    //         await databaseConnection.beast.update(beastId, name, intro, habitat, appearance, senses, diet, meta, sp_atk, sp_def, tactics, size, patreon,
+    //             vitality, panic, stress, lootnotes, effectiveTraitLimit, effectiveRelationshipLimit, effectiveFlawLimit,
+    //             effectivePassionLimit, plural, thumbnail, rarity, combatrole, combatpoints, socialrole, socialpoints, combatsecondary, skillrole, skillpoints, fatigue,
+    //             defaultrole, socialsecondary, notrauma, knockback, singledievitality, noknockback, rolenameorder, descriptionshare, convictionshare, relationshipshare, rollundertrauma,
+    //             imagesource, isincorporeal, weaponbreakagevitality, hasarchetypes, hasmonsterarchetypes, skillsecondary, atk_skill, def_skill, atk_conf, def_conf)
+    //             .catch((error: Error) => sendErrorForward('update main', error, response))
+    //     } else {
+    //         beastId = await databaseConnection.beast.add(userid, name, intro, habitat, appearance, senses, diet, meta, sp_atk, sp_def, tactics, size, patreon,
+    //             vitality, panic, stress, createHash(), lootnotes, effectiveTraitLimit, effectiveRelationshipLimit, effectiveFlawLimit,
+    //             effectivePassionLimit, plural, thumbnail, rarity, combatrole, combatpoints, socialrole, socialpoints, combatsecondary, skillrole, skillpoints, fatigue,
+    //             defaultrole, socialsecondary, notrauma, knockback, singledievitality, noknockback, rolenameorder, descriptionshare, convictionshare, relationshipshare, rollundertrauma,
+    //             imagesource, isincorporeal, weaponbreakagevitality, hasarchetypes, hasmonsterarchetypes, skillsecondary, atk_skill, def_skill, atk_conf, def_conf)
+    //             .catch((error: Error) => sendErrorForward('add main', error, response))[0].id
+    //     }
 
-//     checkForContentTypeBeforeSending(response, { id: beastId })
-// }
+    //     const updateParameters: upsertParameters = {
+    //         roles, types, climates, attacks, defenses, conflicts, skills, movements, variants, specificLoots, pleroma, locationalVitalities, locations, artistInfo, scenarios, folklores,
+    //         casting, deletedSpells, spells, obstacles, challenges, tables, encounters, lairLoot, carriedLoot
+    //     }
+
+    //     if (beastId) {
+    //         await upsertBeast(databaseConnection, beastId, response, updateParameters)
+    //     }
+
+    // ALSO NEED TO UPDATE CACHE IF APPLICABLE
+    checkForContentTypeBeforeSending(response, { beastID })
+    // ALSO UPDATE CACHE ON OTHER SIDE
+}
