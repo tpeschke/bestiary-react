2d9e8e80b7255f443a5dc560a1917e9f7fdfff4d
peschke <mr.peschke@gmail.com>
2025-12-19 07:40:09 -0700

    updating the relationship scaling

--- backend/common/utilities/scalingAndBonus/confrontation/utilities/calculateRelationshipRank.ts
+++ backend/common/utilities/scalingAndBonus/confrontation/utilities/calculateRelationshipRank.ts
@@ -1,7 +1,7 @@
 import getModBySkullIndex from "../../getModBySkullIndex"
 
 export default function calculateRelationshipRank(skullIndex: number = 0, role: string): number {
-    const rankDictionary = [ 1, 2, 3, 4, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44 ]
+    const rankDictionary = [-4, -3, -2, -1, -1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     const roleIndexModifier = getRoleIndexModifier(role)
 
     return getModBySkullIndex(skullIndex, roleIndexModifier, rankDictionary)
