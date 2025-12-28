e29328451956d0785481457efb00e1178793f302
peschke <mr.peschke@gmail.com>
2025-12-28 11:21:26 -0700

    fixing the stress values

--- backend/common/utilities/scalingAndBonus/skill/calculateStress.ts
+++ backend/common/utilities/scalingAndBonus/skill/calculateStress.ts
@@ -4,5 +4,5 @@ import getModBySkullIndex from "../getModBySkullIndex"
 export default function calculateStress(secondaryRole: string, skullIndex: number): number | string {
     const stressDictionary = [2, 3, 4, 5, 6, 8, 10, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57]
 
-    return calculateSecondaryRoleEffect(getModBySkullIndex(skullIndex, skullIndex, stressDictionary), secondaryRole)
+    return calculateSecondaryRoleEffect(getModBySkullIndex(skullIndex, 0, stressDictionary), secondaryRole)
 }
