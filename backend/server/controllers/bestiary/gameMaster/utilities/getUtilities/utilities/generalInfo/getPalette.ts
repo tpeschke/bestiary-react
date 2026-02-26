84f0cf0459c6ba13180a92de0fa6b7619b2633d7
peschke <mr.peschke@gmail.com>
2026-02-25 17:10:21 -0700

    made the linking for common allies two way

--- backend/server/controllers/bestiary/gameMaster/utilities/getUtilities/utilities/generalInfo/getPalette.ts
+++ backend/server/controllers/bestiary/gameMaster/utilities/getUtilities/utilities/generalInfo/getPalette.ts
@@ -3,10 +3,17 @@ import query from "../../../../../../../db/database"
 
 const getPaletteSQL = `select * from bbPalette where beastID = $1`
 
-const getCommonAlliesSQL = `select ca.*, name, plural from bbCommonAllies ca
-join bbIndividualBeast m on m.id = ca.allyID
-where beastID = $1
-order by name`
+const getCommonAlliesSQL = `(
+	select ca.*, name, plural from bbCommonAllies ca
+	join bbIndividualBeast m on m.id = ca.allyID
+	where beastID = $1
+	order by name
+) union (
+	select ca.id, allyID as beastID, beastID as allyID, name, plural from bbCommonAllies ca
+	join bbIndividualBeast m on m.id = ca.beastID
+	where allyID = $1
+	order by name
+)`
 
 export default async function getPalette(beastID: number): Promise<Palette> {
     const [palette] = await query(getPaletteSQL, beastID)
