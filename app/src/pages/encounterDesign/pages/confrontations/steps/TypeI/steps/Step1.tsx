bbd40d1cbb19d0f59a4492b9b4d9e17595700bac
peschke <mr.peschke@gmail.com>
2026-02-27 11:04:39 -0700

    added default emotions to the Confrontation design guidelines

--- app/src/pages/encounterDesign/pages/confrontations/steps/TypeI/steps/Step1.tsx
+++ app/src/pages/encounterDesign/pages/confrontations/steps/TypeI/steps/Step1.tsx
@@ -5,7 +5,16 @@ export default function Step1() {
         <div>
             <img src={emotionsWheel} />
             <p>Review the Emotion Wheel at the beginning of this chapter and choose an Emotion. In 80% of cases, you only need the emotions in the center.</p>
-            <br />
+            <h1>Default Options</h1>
+            <p>The Emotion Wheel can be cool but overwhelming. Truth be told, while it's nice to have all those options, 90% of the time, you can get away with one of the following:</p>
+            <ul>
+                <li>Depression</li>
+                <li>Disgust</li>
+                <li>Fury</li>
+                <li>Joy</li>
+                <li>Surprise</li>
+                <li>Terror</li>
+            </ul>
             <h1 className="italic">Example</h1>
             <p className="italic">I want the swamp to be scary and put the characters on edge so I’m going to select Fearful as the Emotion. I could also choose Scared or even Frightened but Fearful already captured that.</p>
             <p className="italic">Another legitimate choice would have been Nervous.</p>
--- app/src/pages/encounterDesign/pages/confrontations/steps/TypeII/steps/Step2.tsx
+++ app/src/pages/encounterDesign/pages/confrontations/steps/TypeII/steps/Step2.tsx
@@ -9,6 +9,16 @@ export default function Step2() {
             <p>Otherwise, review the Emotion Wheel at the beginning of this chapter and choose an Emotion. In 80% of cases, you only need the emotions in the center.</p>
             <br />
             <p>You’ll also want to note what Emotions the defending side will inflict back should they lose a Check.</p>
+            <h1>Default Options</h1>
+            <p>The Emotion Wheel can be cool but overwhelming. Truth be told, while it's nice to have all those options, 90% of the time, you can get away with one of the following:</p>
+            <ul>
+                <li>Depression</li>
+                <li>Disgust</li>
+                <li>Fury</li>
+                <li>Joy</li>
+                <li>Surprise</li>
+                <li>Terror</li>
+            </ul>
             <h1 className='italic'>Example</h1>
             <p className='italic'>In this case, the players are going to decide the Emotion but I’m going to note that Surprised and Disgusted will simply not work in this case. Anger and Fear might work as long as they’re not centered on the players.</p>
             <p className='italic'>In fact, if the players fail a Check, I think that the captain will get Angry at them.</p>
