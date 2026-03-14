537ac5ed5cf5f7ba403c2b3307d53d48fdde890b
peschke <mr.peschke@gmail.com>
2026-03-14 08:41:08 -0600

    did the mobile stylings for the footer

--- app/src/components/footer/Footer.tsx
+++ app/src/components/footer/Footer.tsx
@@ -4,9 +4,15 @@ export default function Footer() {
     return (
         <>
             <div className='footer-background'>
-                <a href='https://discord.com/invite/G8GVTME' target='_blank'>Discord</a>
-                <a href='https://bonfire.stone-fish.com/' target='_blank'>Bonfire SRD</a>
-                <a href='https://stone-fish.com/' target='_blank'>Stone Fish Games</a>
+                <div>
+                    <a href='https://discord.com/invite/G8GVTME' target='_blank'>Discord</a>
+                </div>
+                <div>
+                    <a href='https://bonfire.stone-fish.com/' target='_blank'>Bonfire SRD</a>
+                </div>
+                <div>
+                    <a href='https://stone-fish.com/' target='_blank'>Stone Fish Games</a>
+                </div>
             </div>
         </>
     )
