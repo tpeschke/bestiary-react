d817c8436ac605ed1d13ac8d191a1eff16426ce9
peschke <mr.peschke@gmail.com>
2026-03-19 17:51:57 -0600

    fixing the relationships ranks

--- backend/common/utilities/scalingAndBonus/bonfire/confrontation/utilities/calculateRelationshipRank.ts
+++ backend/common/utilities/scalingAndBonus/bonfire/confrontation/utilities/calculateRelationshipRank.ts
@@ -1,7 +1,7 @@
 import getModBySkullIndex from "../../../getModBySkullIndex"
 
 export default function calculateRelationshipRank(skullIndex: number = 0, role: string): number {
-    const rankDictionary = [-4, -3, -2, -1, -1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    const rankDictionary = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
     const roleIndexModifier = getRoleIndexModifier(role)
 
     return getModBySkullIndex(skullIndex, roleIndexModifier, rankDictionary)
