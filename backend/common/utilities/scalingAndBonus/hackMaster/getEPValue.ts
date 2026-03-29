9b778d8eabbda5dd0befaef29282a0580f7336b5
peschke <mr.peschke@gmail.com>
2026-03-29 11:58:31 -0600

    updating the 0 skill base ep value

--- backend/common/utilities/scalingAndBonus/hackMaster/getEPValue.ts
+++ backend/common/utilities/scalingAndBonus/hackMaster/getEPValue.ts
@@ -1,5 +1,5 @@
 
 export default function getBaseEPValue(skullValue: number): number {
-    const skullToEPDictionary: number[] = [35, 280, 1015, 1525, 2035, 2540, 3050, 3555]
+    const skullToEPDictionary: number[] = [10, 280, 1015, 1525, 2035, 2540, 3050, 3555]
     return skullToEPDictionary[skullValue]
 }
