35a87140a727e4c93f8392a78e228ae15dc3f3b8
peschke <mr.peschke@gmail.com>
2026-05-24 12:49:51 -0600

    removed a vestigial prop

--- app/src/pages/bestiary/beast/pages/view/gmView/components/leftColumn/components/specialInfo/specialInfo.tsx
+++ app/src/pages/bestiary/beast/pages/view/gmView/components/leftColumn/components/specialInfo/specialInfo.tsx
@@ -1,6 +1,5 @@
 import Body from "../../../../../../../components/UI/body/Body";
 import HTMLDisplay from "../../../../../../../components/UI/htmlDisplay/htmlDisplay";
-import { SystemOption } from "@bestiary/common/interfaces/beast/beast";
 
 interface Props {
     info?: string,
