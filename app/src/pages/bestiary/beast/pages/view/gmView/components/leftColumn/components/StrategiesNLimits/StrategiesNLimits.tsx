8af9252e3bb46b944d067669068035052ea42fce
peschke <mr.peschke@gmail.com>
2026-06-07 07:34:15 -0600

    added a small subtitle to the strategy section to make things more obvious

--- app/src/pages/bestiary/beast/pages/view/gmView/components/leftColumn/components/StrategiesNLimits/StrategiesNLimits.tsx
+++ app/src/pages/bestiary/beast/pages/view/gmView/components/leftColumn/components/StrategiesNLimits/StrategiesNLimits.tsx
@@ -13,6 +13,7 @@ interface Props {
 
 export default function StrategiesNLimits({ strategiesNLimits, limitNotes }: Props) {
     const systemPreference = useSelector(getSystemPreference) as 0 | 1 | 2 | undefined
+    const isBonfire = systemPreference === BONFIRE
 
     function formatDiminish(diminish: number | null) {
         if (systemPreference === HACKMASTER) {
@@ -58,8 +59,8 @@ export default function StrategiesNLimits({ strategiesNLimits, limitNotes }: Pro
 
     const chaosNDiminishTooltip = 'Chaos and Diminish limits are explained in R.2 on the SRD.'
 
-    const chaosTooltip = systemPreference === BONFIRE ? chaosNDiminishTooltip : 'This is the number of enemies that need to be killed to trigger a Diminish Check.'
-    const diminishTooltip = systemPreference === BONFIRE ? chaosNDiminishTooltip : 'This is the chance of the Diminish Check being successful.'
+    const chaosTooltip = isBonfire ? chaosNDiminishTooltip : 'This is the number of enemies that need to be killed to trigger a Diminish Check.'
+    const diminishTooltip = isBonfire ? chaosNDiminishTooltip : 'This is the chance of the Diminish Check being successful.'
 
     const strategiesTooltip = 'This is the base number of Strategies that this group can employ in response to the players\'s actions. See below for options.'
     const baselineStrategiesTooltip = 'This is base number of Strategies that this group has already employed in / near their base of operations.'
@@ -73,50 +74,57 @@ export default function StrategiesNLimits({ strategiesNLimits, limitNotes }: Pro
         <>
             <br />
             <h2 className="border">Strategies & Limits</h2>
+            {!isBonfire && <em>Hover the title of each column to learn more about it.</em>}
             <Body>
-                <>
-                    <table className="light-weight-table">
-                        <thead>
-                            <tr>
-                                <th>Group</th>
-                                <th data-tooltip-id="my-tooltip" data-tooltip-content={chaosTooltip} className="center-text">
+                <table className="light-weight-table">
+                    <thead>
+                        <tr>
+                            <th>Group</th>
+                            <th data-tooltip-id="my-tooltip" data-tooltip-content={chaosTooltip} className="center-text">
+                                {isBonfire ? (
                                     <a href="https://bonfire.stone-fish.com/rules/2#gm-during-rests" target="_blank">
                                         Chaos
                                     </a>
-                                </th>
-                                <th data-tooltip-id="my-tooltip" data-tooltip-content={diminishTooltip} className="center-text">
+                                ) : (
+                                    'Chaos'
+                                )}
+                            </th>
+                            <th data-tooltip-id="my-tooltip" data-tooltip-content={diminishTooltip} className="center-text">
+                                {isBonfire ? (
                                     <a href="https://bonfire.stone-fish.com/rules/2#gm-during-rests" target="_blank">
                                         Diminish
                                     </a>
-                                </th>
-                                <th data-tooltip-id="my-tooltip" data-tooltip-content={strategiesTooltip} className="center-text">Strategies</th>
-                                <th data-tooltip-id="my-tooltip" data-tooltip-content={baselineStrategiesTooltip} className="center-text">Baseline Strategies</th>
-                                <th data-tooltip-id="my-tooltip" data-tooltip-content={treasureTooltip}>
-                                    <Link to="/treasure">
-                                        Treasure
-                                    </Link>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {strategiesNLimits && strategiesNLimits.map(({ group, chaos, chaosNote, diminish, strategies, strategiesNote, baselineStrategies, treasure }, index) => {
-                                return (
-                                    <tr key={index} >
-                                        <td>{group}</td>
-                                        <td className="center-text">{returnDashIfNull(chaos)}{chaosNote ? '*' : ''}</td>
-                                        <td className="center-text">{formatDiminish(diminish)}</td>
-                                        <td className="center-text">{returnDashIfNull(strategies)}{strategiesNote ? chaosNote ? '**' : '*' : ''}</td>
-                                        <td className="center-text">{returnDashIfNull(baselineStrategies)}</td>
-                                        <td>{returnDashIfNull(treasure)}</td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
+                                ) : (
+                                    'Diminish'
+                                )}
+                            </th>
+                            <th data-tooltip-id="my-tooltip" data-tooltip-content={strategiesTooltip} className="center-text">Strategies</th>
+                            <th data-tooltip-id="my-tooltip" data-tooltip-content={baselineStrategiesTooltip} className="center-text">Baseline Strategies</th>
+                            <th data-tooltip-id="my-tooltip" data-tooltip-content={treasureTooltip}>
+                                <Link to="/treasure">
+                                    Treasure
+                                </Link>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {strategiesNLimits && strategiesNLimits.map(({ group, chaos, chaosNote, diminish, strategies, strategiesNote, baselineStrategies, treasure }, index) => {
+                            return (
+                                <tr key={index} >
+                                    <td>{group}</td>
+                                    <td className="center-text">{returnDashIfNull(chaos)}{chaosNote ? '*' : ''}</td>
+                                    <td className="center-text">{formatDiminish(diminish)}</td>
+                                    <td className="center-text">{returnDashIfNull(strategies)}{strategiesNote ? chaosNote ? '**' : '*' : ''}</td>
+                                    <td className="center-text">{returnDashIfNull(baselineStrategies)}</td>
+                                    <td>{returnDashIfNull(treasure)}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
 
-                    <br />
-                    <HTMLDisplay html={limitNotes} />
-                </>
+                <br />
+                <HTMLDisplay html={limitNotes} />
             </Body>
         </>
     )
