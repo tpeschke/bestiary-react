5c727b5e3a6d314d718a27c2b551e45cafec1160
peschke <mr.peschke@gmail.com>
2026-06-08 13:46:05 -0600

    updating hm skill rank dictionaries

--- backend/common/utilities/scalingAndBonus/bonfire/confrontation/utilities/calculateDescriptionRank.ts
+++ backend/common/utilities/scalingAndBonus/bonfire/confrontation/utilities/calculateDescriptionRank.ts
@@ -2,7 +2,7 @@ import { SystemOption } from "../../../../../interfaces/beast/beast"
 import getModBySkullIndex from "../../../getModBySkullIndex"
 
 const bonfireRankDictionary = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7]
-const hackMasterRankDictionary = [ -100, -90, -85, -80, -75, -65, -60, -55, -50, -40, -35, -25, -20, -15, -10, 0, 10, 15, 20, 30, 35, 40, 50, 55, 60, 70, 75, 80, 90, 95, 100, 110, 115, 120, 130, 135, 140, 150, 155, 160, 170 ]
+const hackMasterRankDictionary = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180]
 
 
 export default function calculateDescriptionRank(skullIndex: number = 0, system: SystemOption): number {
--- backend/common/utilities/scalingAndBonus/bonfire/skill/getSkillRank.ts
+++ backend/common/utilities/scalingAndBonus/bonfire/skill/getSkillRank.ts
@@ -1,8 +1,8 @@
 import getModBySkullIndex from "@bestiary/common/utilities/scalingAndBonus/getModBySkullIndex"
 import { SystemOption } from "../../../../interfaces/beast/beast"
 
-const bonfireRankDictionary = [ -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36 ]
-const hackMasterRankDictionary = [ -100, -90, -85, -80, -75, -65, -60, -55, -50, -40, -35, -25, -20, -15, -10, 0, 10, 15, 20, 30, 35, 40, 50, 55, 60, 70, 75, 80, 90, 95, 100, 110, 115, 120, 130, 135, 140, 150, 155, 160, 170 ]
+const bonfireRankDictionary = [-8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
+const hackMasterRankDictionary = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180]
 
 export default function getBaseSkillRank(skullIndex: number = 0, system: SystemOption): number {
     const rankDictionary = system === 'Bonfire' ? bonfireRankDictionary : hackMasterRankDictionary
