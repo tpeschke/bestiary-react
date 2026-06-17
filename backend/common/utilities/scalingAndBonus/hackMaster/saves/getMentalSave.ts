9d80cdb1133fa86d3f8fc5f71aa46cbe0fc3df1c
peschke <mr.peschke@gmail.com>
2026-06-17 06:27:18 -0600

    making save values more extreme

--- backend/common/utilities/scalingAndBonus/hackMaster/saves/getMentalSave.ts
+++ backend/common/utilities/scalingAndBonus/hackMaster/saves/getMentalSave.ts
@@ -2,7 +2,7 @@ import getModBySkullIndex from "../../getModBySkullIndex"
 
 export default function getMentalSave(skullIndex: number): string {
     const saveDictionary = [
-        -4, -3, -2, -1, 0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 24, 25, 26, 27, 27, 28, 29, 30, 31, 32
+        -5, -4, -3, -1, 0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15, 16, 17, 19, 20, 21, 23, 24, 25, 27, 28, 29, 31, 32, 33, 35, 36, 37, 38, 40, 41, 42, 44, 45, 46, 48
     ]
 
     const rank = getModBySkullIndex(skullIndex, 0, saveDictionary)
--- backend/common/utilities/scalingAndBonus/hackMaster/saves/getPhysicalSave.ts
+++ backend/common/utilities/scalingAndBonus/hackMaster/saves/getPhysicalSave.ts
@@ -2,7 +2,7 @@ import getModBySkullIndex from "../../getModBySkullIndex"
 
 export default function getPhysicalSave(skullIndex: number): string {
     const saveDictionary = [
-        -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 36, 37, 38, 39
+        -6, -5, -3, -2, 0, 2, 3, 5, 6, 8, 10, 11, 13, 15, 16, 18, 19, 21, 23, 24, 26, 27, 29, 31, 32, 34, 36, 37, 39, 40, 42, 44, 45, 47, 48, 50, 52, 53, 55, 57, 58
     ]
 
     const rank = getModBySkullIndex(skullIndex, 0, saveDictionary)
